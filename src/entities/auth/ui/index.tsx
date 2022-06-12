f7e8fe9b25211d66baf40b4070bd8f091a442ab1
Vitaliy Kuroedov <wilde@bk.ru>
2022-06-12 15:26:37 +0300

    auth form rework

--- src/entities/auth/ui/index.tsx
+++ src/entities/auth/ui/index.tsx
@@ -1,6 +1,7 @@
+import { Transition } from "@headlessui/react"
 import { useField, useForm } from "effector-forms"
 import { useEvent } from "effector-react"
-import { InputHTMLAttributes, ReactNode, memo, ChangeEvent, useCallback, FormEvent } from "react"
+import { InputHTMLAttributes, ReactNode, memo, ChangeEvent, useCallback, FormEvent, Fragment } from "react"
 import { UserInput } from "shared/ui/user-input"
 import { authModel } from ".."
 
@@ -13,40 +14,39 @@ export const AuthForm = () => {
     }
 
     return (
-        <form onSubmit={onSubmit} className="flex w-full flex-col space-y-8">
-            <UserEmailField />
-            <UserPasswordField />
-            <div className="!my-4 flex justify-between text-sm text-gray-400">
-                <button className="hover:text-gray-900" type="button">
-                    запомнить
-                </button>
-                <button className="hover:text-gray-900" type="button">
-                    забыли пароль?
-                </button>
-            </div>
+        <Transition
+            appear
+            show={true}
+            enter="transition ease-in-out duration-700  transition-opacity"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition ease-in-out duration-700  transition-opacity"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            as={Fragment}
+        >
+            <form onSubmit={onSubmit} className="flex w-full flex-col space-y-8">
+                <UserEmailField />
+                <UserPasswordField />
+                <div className="!my-2 flex justify-between text-sm text-gray-400">
+                    <button className="hover:text-gray-900" type="button">
+                        запомнить
+                    </button>
+                    <button className="hover:text-gray-900" type="button">
+                        забыли пароль?
+                    </button>
+                </div>
 
-            <button
-                type="submit"
-                className="flex justify-center  rounded-lg bg-cyan-600 px-8 py-4 text-sm uppercase text-white duration-150 hover:bg-green-500"
-            >
-                вход
-            </button>
-        </form>
+                <button
+                    type="submit"
+                    className="flex justify-center  rounded-lg bg-cyan-600 px-8 py-4 text-sm uppercase text-white duration-150 hover:bg-green-500"
+                >
+                    вход
+                </button>
+            </form>
+        </Transition>
     )
 }
-
-interface InputFiledProps extends InputHTMLAttributes<HTMLInputElement> {
-    label?: string | ReactNode
-}
-
-const InputFiled = memo(({ label, ...props }: InputFiledProps) => {
-    return (
-        <label className="flex flex-col space-y-2 text-gray-900">
-            <span>{label}</span>
-            <input {...props} className="rounded-lg border-2 p-2" />
-        </label>
-    )
-})
 
 export const RegistrationForm = () => {
     const credential = authModel.selectors.useRegistrationCredential()
@@ -55,43 +55,48 @@ export const RegistrationForm = () => {
     const handleSubmit = useEvent(authModel.events.registration)
 
     return (
-        <form onSubmit={handleSubmit} className="flex w-full flex-col space-y-4 ">
-            <UserInput
-                value={credential.email}
-                onChange={handleChange}
-                errorText={() => ""}
-                placeholder="почта"
-                name="почта"
-            />
-            <UserInput
-                value={credential.name}
-                onChange={handleChange}
-                errorText={() => ""}
-                placeholder="имя"
-                name="имя"
-            />
-            <UserInput
-                value={credential.password}
-                onChange={handleChange}
-                errorText={() => ""}
-                placeholder="пароль"
-                name="пароль"
-            />
-            <UserInput
-                value={credential.password}
-                onChange={handleChange}
-                errorText={() => ""}
-                placeholder="повтор пароля"
-                name="повтор пароля"
-            />
+        <Transition
+            appear
+            show={true}
+            enter="transition ease-in-out duration-700  transition-opacity"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition ease-in-out duration-700  transition-opacity"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            as={Fragment}
+        >
+            <form onSubmit={handleSubmit} className="flex w-full flex-col space-y-4 ">
+                <UserInput
+                    value={credential.email}
+                    onChange={handleChange}
+                    errorText={() => ""}
+                    placeholder="почта"
+                    name="почта"
+                />
+                <UserInput
+                    value={credential.name}
+                    onChange={handleChange}
+                    errorText={() => ""}
+                    placeholder="имя"
+                    name="имя"
+                />
+                <UserInput
+                    value={credential.password}
+                    onChange={handleChange}
+                    errorText={() => ""}
+                    placeholder="пароль"
+                    name="пароль"
+                />
 
-            <button
-                type="submit"
-                className="flex justify-center  rounded-lg bg-cyan-600 px-8 py-4 text-sm uppercase text-white duration-150 hover:bg-green-500"
-            >
-                регистрация
-            </button>
-        </form>
+                <button
+                    type="submit"
+                    className="flex justify-center  rounded-lg bg-cyan-600 px-8 py-4 text-sm uppercase text-white duration-150 hover:bg-green-500"
+                >
+                    регистрация
+                </button>
+            </form>
+        </Transition>
     )
 }
 
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,10 @@
-import { UserIcon } from "@heroicons/react/solid"
-import { AuthForm, authModel, RegistrationForm } from "entities/auth"
-import { useMemo } from "react"
+import clsx from "clsx"
+import { useMemo, useRef } from "react"
 import { Location, Navigate, NavLink, useLocation, useSearchParams } from "react-router-dom"
+
+import { AuthForm, authModel, RegistrationForm } from "entities/auth"
+
+import { UserIcon } from "@heroicons/react/solid"
 
 export const AuthPage = () => {
     const isAuth = authModel.selectors.useIsAuth()
@@ -17,11 +20,18 @@ export const AuthPage = () => {
 
     if (isAuth) return <Navigate to={fromPage} replace />
 
+    const ref = useRef<HTMLDivElement>(null)
+
     return (
-        <div className="flex grow flex-col items-center space-y-2 bg-gradient-to-tr from-green-600 to-cyan-500 px-4 py-2 font-sans md:space-y-4 md:px-10 md:pb-5 md:pt-20">
-            <div className="relative flex w-full max-w-md flex-col items-center  rounded-lg bg-white px-10 shadow-lg">
-                <UserIcon className="absolute h-32 w-32 -translate-y-1/2 rounded-full border-[8px] border-teal-500 bg-cyan-600 p-6 text-white ring-[8px] ring-white " />
-                <h2 className="mt-16 mb-4 text-2xl font-semibold uppercase text-gray-600">
+        <div
+            ref={ref}
+            className="relative flex grow flex-col items-center space-y-2 px-4 py-2 font-sans  md:space-y-4 md:px-10 md:pb-5 md:pt-20"
+        >
+            <BGSliceGradient />
+
+            <div className="group relative flex w-full max-w-md flex-col items-center  rounded-lg bg-white/70 px-10 shadow-lg hover:bg-white">
+                <UserIcon className="absolute h-32 w-32 -translate-y-1/2 rounded-full border-[8px] border-teal-500 bg-cyan-600 p-6 text-white ring-[8px] ring-white duration-300 group-hover:-translate-x-[calc(100%-1rem)] " />
+                <h2 className="mt-20 mb-4 text-2xl font-semibold uppercase text-gray-600">
                     {!currentWindow ? "регистрация" : "вход"}
                 </h2>
                 {currentWindow ? <AuthForm /> : <RegistrationForm />}
@@ -34,6 +44,24 @@ export const AuthPage = () => {
                     {currentWindow ? "регистрация" : "вход"}
                 </NavLink>
             </div>
+        </div>
+    )
+}
+
+const BGSliceGradient = () => {
+    return (
+        <div className="absolute inset-0 flex overflow-hidden bg-cyan-800  [&>*]:border-l [&>*]:border-slate-400/30 ">
+            {[...Array(5).keys()]
+                .map((i) => i + 1)
+                .map((item) => (
+                    <span
+                        key={item}
+                        className={clsx(
+                            "flex-1 skew-x-[35deg]  from-green-800 via-green-800/50 to-cyan-800 hover:bg-gradient-to-r ",
+                            item === 5 && "border-r"
+                        )}
+                    ></span>
+                ))}
         </div>
     )
 }
